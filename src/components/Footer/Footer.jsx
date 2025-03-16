a8a4380df599a20045212c80f9bb87384547897c
Enrico B. Palma <enricob.palma@gmail.com>
2025-03-16 01:55:55 -0300

    Adds Youtube Logo to Project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import agotechLogo from "../../assets/agriculture.svg"
+import { YoutubeLogo } from "@phosphor-icons/react"
 import './style.css'
 
 function Footer() { 
     return (
         <>
             <div className="row p-3 bg-secondary shadow-sm mt-auto">
-                <div className="col-6">
+                <div className="col-5">
                     <div className="d-flex align-items-center">
                         <Link to="/">
                             <img src={agotechLogo} alt="Logo Agrotech" className="me-3 main-logo" />
@@ -15,7 +16,7 @@ function Footer() {
                     </div>
                     <p className="mt-3 text-color">© Copyright 2024 Agrotech</p>
                 </div>
-                <div className="col-4">                
+                <div className="col-5">                
                     <ul>
                         <li>
                             <Link to="/contact" className="text-color">
@@ -39,9 +40,13 @@ function Footer() {
                         </li>
                     </ul>
                 </div>            
+                <div className="col-2">
+                    <a href="https://youtube.com.br/" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="80" viewBox="0 0 256 180"><path fill="#f00" d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"/><path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z"/></svg>
+                    </a>
+                </div>
             </div>
         </>
     )
 }
-
 export default Footer
